80d8e02932f95b447343175df92e6a4d08953053
John Prutton <johnrprutton@gmail.com>
2026-03-21 20:20:33 +0200

    feat(frontend): add shadcn

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router"
 
+import { Button } from "@/components/ui/button"
+
 export const Route = createFileRoute("/")({ component: App })
 
 function App() {
-	return <main>testing</main>
+	return (
+		<main className="h-screen grid place-content-center">
+			<Button size={"lg"}>PLAY</Button>
+		</main>
+	)
 }
